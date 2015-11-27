95d55facb872995c6ca74ee9fbea282825671e5b
ilaunchpad <sameer.raut@gmail.com>
2015-11-26 23:25:41 -0600

    Restart of the project

--- inbox_cleaner.py
+++ inbox_cleaner.py
@@ -1,19 +1,12 @@
 __author__ = 'rauts3'
-from pygmail import pygmail
 
-
-def _get_unread_count():
-
-     new_email = pygmail()
-     rc = new_email.login('username', 'passwd')
-     if rc == 'OK':
-        count = new_email.get_mail_count()
-     return count
+import imaplib, re
 
 
 
-unread_count = _get_unread_count()
-print(unread_count)
+
+
+
 
 
 
