e45f20c8a86eb520e365d9bc6eede3479b62f566
ilaunchpad <sameer.raut@gmail.com>
2015-11-28 12:13:25 -0600

    functions to search for email and delete from gmail

--- inbox_cleaner.py
+++ inbox_cleaner.py
@@ -1,6 +1,6 @@
 __author__ = 'rauts3'
 
-import imaplib, re
+import imaplib, sys
 global imap_conxn, auth
 
 
@@ -12,10 +12,7 @@ def _getconxn_():
 def _login_(username, passwd):
     global auth
     auth, resp = imap_conxn.login(username, passwd)
-    return resp
 
-def _getmailcount_():
-    pass
 def _getmailbox_():
     mailbox = []
     if auth == 'OK':
@@ -26,12 +23,60 @@ def _getmailbox_():
             mailbox.append(str_item)
     return mailbox
 
+def _searchmail_(tag, mailbox= None):
+
+    if auth == 'OK':
+
+       if mailbox is None:
+            mailbox = 'Inbox'
+            imap_conxn.select(mailbox)
+            resp, count = imap_conxn.search(None, tag)
+       else:
+            imap_conxn.select(mailbox)
+            resp, count = imap_conxn.search(None,tag )
+
+       uid = count[0].decode()
+       if uid:
+            li_uid = uid.split()
+            return li_uid
+
+def _deletemail_(li_uid):
+
+    for item in li_uid:
+        imap_conxn.store(item,'+FLAGS', '\\Deleted')
+    imap_conxn.expunge()
 
 
-_getconxn_()
-_login_('sameer.raut', 'noshan')
-list = _getmailbox_()
-print(list)
+if __name__ == "__main__":
+    _getconxn_()
+    if imap_conxn:
+        username = input("Enter username: ")
+        passwd = input("Enter passwd: ")
+        _login_(username, passwd)
+        if auth == 'OK':
+            mailbox = _getmailbox_()
+            search_tag = input("Enter search tag :")
+            format_tag = '(from '+ search_tag +')'
+            li_uid = _searchmail_(format_tag)
+            if li_uid:
+                _deletemail_(li_uid)
+                print("Task executed")
+            else:
+                print("Search not found")
+    imap_conxn.close()
+    imap_conxn.logout()
+    print('Program Terminated')
+    sys.exit()
+
+
+
+
+
+
+
+
+
+
 
 
 
